3861b13bfe0ced70458ebf0a73d6b4ad73032256
Mahi <agrawalmahi18012006@gmail.com>
2026-03-31 11:37:33 +0530

    Updated project files and added code

--- backend/src/controllers/todo.controller.js
+++ backend/src/controllers/todo.controller.js
@@ -19,4 +19,31 @@ const getTodos=async(req,res)=>{
     }
 };
 
-module.exports={ createTodo, getTodos };
+const updateTodo = async(req,res) => {
+    try{
+        const{ id } = req.params;
+        const updatedTodo = await Todo.findByIdAndUpdate(id, req.body);
+
+        res.status(200).json({ message: "todo updated", data: updatedTodo})
+    }catch(error){
+        res.status(500).json({ error: error.message});
+    }
+};
+
+const deleteTodo = async(req,res) => {
+    try{
+        const{ id } = req.params;
+        const todo = await Todo.findByIdAndDelete(id);
+
+        res.status(200).json({ message: "deleted successfully"})
+    }catch(error){
+        res.status(500).json({ error: error.message});
+    }
+};
+
+module.exports = { 
+    createTodo, 
+    getTodos, 
+    updateTodo, 
+    deleteTodo 
+};
--- backend/src/routes/todo.routes.js
+++ backend/src/routes/todo.routes.js
@@ -1,9 +1,13 @@
 const express=require("express");
 
 const router=express.Router();
-const{getTodos,createTodo}=require("../controllers/todo.controller")
+const{getTodos,createTodo,updateTodo,deleteTodo}=require("../controllers/todo.controller")
+const showLog = require("../middleware/log.middleware");
+const { validateTodo } = require("../middleware/validateTodo.middleware");
 
-router.get("/",getTodos)
-router.post("/",createTodo)
+router.get("/", showLog ,getTodos)
+router.post("/",validateTodo,createTodo)
+router.put("/:id",updateTodo);
+router.delete("/:id",deleteTodo);
 
 module.exports=router;
