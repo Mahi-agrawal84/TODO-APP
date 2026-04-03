093a10039741fb1d845b76b6e237b5e57fdfe571
Mahi <agrawalmahi18012006@gmail.com>
2026-04-03 15:46:32 +0530

    add auth middleware

--- backend/src/routes/todo.routes.js
+++ backend/src/routes/todo.routes.js
@@ -4,10 +4,11 @@ const router=express.Router();
 const{getTodos,createTodo,updateTodo,deleteTodo}=require("../controllers/todo.controller")
 const showLog = require("../middleware/log.middleware");
 const { validateTodo } = require("../middleware/validateTodo.middleware");
+const auth = require("../middleware/auth.middleware");
 
 router.get("/", showLog ,getTodos)
-router.post("/",validateTodo,createTodo)
-router.put("/:id",updateTodo);
-router.delete("/:id",deleteTodo);
+router.post("/",auth,validateTodo,createTodo)
+router.put("/:id",auth,updateTodo);
+router.delete("/:id",auth,deleteTodo);
 
-module.exports=router;
+module.exports=router; 
